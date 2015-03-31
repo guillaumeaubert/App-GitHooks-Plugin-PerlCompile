9254c30a383ebf3dfcf6a93e7682ece95f05b017
Guillaume Aubert <aubertg@cpan.org>
2015-03-30 21:28:18 -0700

    Release version 1.1.0.

--- lib/App/GitHooks/Plugin/PerlCompile.pm
+++ lib/App/GitHooks/Plugin/PerlCompile.pm
@@ -26,11 +26,11 @@ allowing the commit to be completed.
 
 =head1 VERSION
 
-Version 1.0.2
+Version 1.1.0
 
 =cut
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.1.0';
 
 
 =head1 CONFIGURATION OPTIONS
